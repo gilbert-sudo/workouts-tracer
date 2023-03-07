09d90e8cf94ddd50c83fda2f712b6aa0edcdc4f4
Ranaivonampoizina Mikajy <mikamickswonder@gmail.com>
2023-03-07 11:31:39 +0100

    try the proxy

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -10,7 +10,7 @@ export const useSignup = () => {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch("/api/user/signup", {
+    const response = await fetch("https://workoutstracer-api.onrender.com/api/user/signup", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
